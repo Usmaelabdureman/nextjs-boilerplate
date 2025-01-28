ecae90a7992f00af27054d60e920fd4573651333
Usmaelabdureman <uabdureman@gmail.com>
2025-01-28 12:10:50 +0300

    removed unnecessary component

--- components/Footer.tsx
+++
@@ -1,9 +0,0 @@
-
-const Footer = () => {
-    return (
-        <div className='flex justify-between items-center p-5 bg-[#FFAE00]'> 
-            <p>ffffff</p>
-            <p>Privacy Policy | Terms of Service</p>
-        </div>
-    );
-}
